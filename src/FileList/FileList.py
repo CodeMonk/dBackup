428ced683b6b9ff23ef9ddd91eedca98123bd20a
David Frascone <dave@frascone.com>
2015-01-30 23:44:54 -0700

    File searching now working, tested, and unit tested.  For now, we're only gathering a list of files.  We could think about pre-gathering the stats stuff -- but -- we have to walk our file list anyway, so . . .

--- src/FileList/FileList.py
+++ src/FileList/FileList.py
@@ -7,6 +7,10 @@ directories, and will produce a list of files.
 
 """
 
+import os
+import re
+
+
 class FileList():
 	"""
 	Produce a list of files, from specified roots and include / exclude lists
@@ -15,10 +19,55 @@ class FileList():
 	def __init__(self, directories=[], exclude_regexps=[]):
 		""" Constructor """
 		self.directories = directories
-		self.exclude_regexps = exclude_regexps
+		self.exclude_regexps = self.__preCompile(exclude_regexps)
+
+	def __preCompile(self, regexps):
+		""" Precompile our list of regular expressions for speed later"""
+		result = []
+		for regexp in regexps:
+			result.append(re.compile(regexp))
+
+		return result
+
+	def __getFilesFromDirectory(self, dir):
+		"""
+		Recursively return all files from the Directory
+		"""
+		files=[]
+		for root, dirnames, filenames in os.walk(dir):
+			for filename in filenames:
+				files.append(os.path.join(root,filename))
+		return files
+
+	def __matchesAny(self, string, regexps):
+		"""
+		Return true if string matches any of the regexps
+		"""
+		for reg in regexps:
+			if reg.search(string):
+				return True
+		return False
+
+	def __scrubFiles(self, files, excludes):
+		"""
+		Remove any files from list that match excludes, and return the
+		scrubbed array.
+		"""
+
+		result=[]
+
+		for file in files:
+			if not self.__matchesAny(file, excludes):
+				result.append(file)
+
+		return result
 
 
 	def getFiles(self):
 		""" Return an array of the files found matching our parameters """
+		self.files=[]
+		for dir in self.directories:
+			dir_files = self.__getFilesFromDirectory(dir)
+			self.files += self.__scrubFiles(dir_files, self.exclude_regexps)
 
-		return []
+		return self.files
--- src/FileList/test_FileList.py
+++ src/FileList/test_FileList.py
@@ -5,6 +5,7 @@ Unittests for File List
 
 """
 import unittest
+import os
 
 from FileList import FileList
 
@@ -25,8 +26,56 @@ class TestFileList(unittest.TestCase):
 		fl = FileList(directories=directories)
 		self.assertEqual(directories, fl.directories)
 
+	def test_fileSearch(self):
+		"""
+		Do some searches on the local directory -- this will fail if
+		files are added or deleted.
+		"""
+
+		# Find python files (2) (minus pyc)
+		# Find all files >= python files.
+
+		allFL=FileList(directories=['.'])
+		noPycFL=FileList(directories=['.'], exclude_regexps=[r'\.pyc$'])
+
+		allFiles=allFL.getFiles()
+		noPycFiles=noPycFL.getFiles()
+
+		# We should have two non .pyc files
+		self.assertEqual(len(noPycFiles), 2)
+		# We could have some pyc files
+		self.assertTrue(len(noPycFiles) <= len(allFiles))
+
+		# Make sure our Module and test were found.
+		self.assertIn(os.path.join('.', 'FileList.py'), allFiles)
+		self.assertIn(os.path.join('.','test_FileList.py'),allFiles)
+		self.assertIn(os.path.join('.', 'FileList.py'), noPycFiles)
+		self.assertIn(os.path.join('.','test_FileList.py'),noPycFiles)
+
+
+
 
 if __name__ == '__main__':
 	unittest.main()
+	# import os, re
+
+	# # Let's create some file lists
+	# print "All Files:"
+	# fl=FileList(directories=['.'])
+	# print fl.getFiles()
+
+	# print "\nMinus .pyc"
+	# fl=FileList(directories=['.'], exclude_regexps=[r'\.pyc$'])
+	# print fl.getFiles()
+
+	# sep=re.escape(os.sep)
+	# test_exp = "{sep}test_[^{sep}]*$".format(sep=sep)
+	# print "\nMinus tests (re={})".format(test_exp)
+	# fl=FileList(directories=['.'], exclude_regexps=[test_exp])
+	# print fl.getFiles()
+
+	# print "\n/tmp"
+	# fl=FileList(directories=['/tmp'])
+	# print fl.getFiles()
 
 		
